8b17bb5eac9660fb78459627b4520aa100befbdf
eigeen <dengyk2002@qq.com>
2021-01-16 17:10:07 +0800

    修复版本号不正确的bug v0.0.3

--- bili_luckydraw/info.py
+++ bili_luckydraw/info.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-_version_ = "0.0.2"
+_version_ = "0.0.3"
 _description_ = "Scrape forwarded users from bilibili.com."
 _requires_ = ['requests', 'retrying', 'xlwt']
